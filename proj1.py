10f0acd148385bd37b135cadf800bc6ab39d92c3
Markus Shaw <mashaw@csumb.edu>
2015-09-02 18:49:05 -0700

    Added .py folder with beginning project code

---
+++ proj1.py
@@ -0,0 +1,27 @@
+import os.path
+path = "C:\Users\Markus\School\CST 205\projects\proj1\cst205_proj1\Project1Images"
+
+numfiles = len([f for f in os.listdir(path)
+                if os.path.isfile(os.path.join(path,f))])
+print(numfiles)
+
+image_paths = os.listdir(path)
+
+print(image_paths)
+
+mypicture = image_paths[0]
+
+pic = makePicture(path + "\\" + image_paths[0])
+
+#show(pic)
+pics = []
+limit = 0
+while( limit < 9 ):
+  pic = makePicture(path + "\\" + image_paths[limit])
+  pics.append(pic)
+  show(pic)
+  limit+=1
+print(pics)
+
+
+  
