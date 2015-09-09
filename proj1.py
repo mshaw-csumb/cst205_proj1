d49d14d8ba87da9382bd8c2f7e5ee5991a8e98ad
Markus Shaw <mashaw@csumb.edu>
2015-09-09 11:47:47 -0700

    More progress

--- proj1.py
+++ proj1.py
@@ -1,9 +1,12 @@
-import os.path
+#must use getPixel(Picture,xpos,ypos) method, will help much
+
+import os
+#import os.path
 path = pickAFolder()
 
-numfiles = len([f for f in os.listdir(path)#find reference link from stack overflow
-                if os.path.isfile(os.path.join(path,f))])
-print(numfiles)
+#numfiles = len([f for f in os.listdir(path)#find reference link from stack overflow
+               # if os.path.isfile(os.path.join(path,f))])
+#print(numfiles)
 
 image_paths = os.listdir(path)
 
@@ -23,13 +26,43 @@ while( limit < 9 ):
   limit+=1
 #print(pics)
 
-show(pics[1])
+#show(pics[1])
 
 i = 0
 pixels = []
 while(i < 9):
   pixels.append(pics[i].getPixels())
   i = i+1
+x = 0
+y = 0;
+height = pics[0].getHeight()
+width = pics[0].getWidth()
+
+
+while (x < width):  
+  while(y < height):
+    pixel = getPixel(pics[0],x,y)
+    if(pixel.getGreen() > 50):
+      setGreen(pixel,0)
+    y = y + 1
+  x = x + 1
+  y = 0
+   
+   
+
+while(row < x):
+  while (col < y and row < x):
+    pixel = getPixel(pics[0],row,col)
+    if (pixel.getBlue() < 50):
+      setBlue(pixel,0)
+    col = col + 1
+    
+  row = row + 1
+  
+      
+      
+      
+explore(pics[0])
 
 #print(pixels[1][:4])
 #pixels[1][0] = pixels[1][2]
@@ -38,9 +71,4 @@ while(i < 9):
 
 #pixels[1][0:20] = pixels[1][90:110]
 
-setRed(pixels[1][0],pixels[1][1000].getRed())
-setBlue(pixels[1][0],pixels[1][1000].getBlue())
-setGreen(pixels[1][0],pixels[1][1000].getGreen())
-repaint(pics[1])
-explore(pics[1])
-print(pixels[1][:4])
+
